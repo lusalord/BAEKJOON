f45dc875fa5b327e4ea4ab28615b045ca5a5b8e3
lusalord <127508413+lusalord@users.noreply.github.com>
2025-03-13 10:02:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Bulgi = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Bulgi - 543);
+        }
+    }
+}
