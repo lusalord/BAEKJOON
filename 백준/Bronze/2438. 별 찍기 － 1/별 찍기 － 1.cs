e8705f6fae890813696c803e20bb2230b1c89201
lusalord <127508413+lusalord@users.noreply.github.com>
2025-03-13 10:49:26 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 196 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Line = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= Line; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
