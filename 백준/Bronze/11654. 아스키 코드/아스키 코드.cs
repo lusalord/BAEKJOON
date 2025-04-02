b320284b5f567c0c466358d6326e9f097f6b6c69
lusalord <127508413+lusalord@users.noreply.github.com>
2025-04-02 21:32:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 52 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Convert.ToChar(Console.ReadLine());
+            int x = Convert.ToInt32(a);
+            Console.WriteLine(x);
+        }
+    }
+}
