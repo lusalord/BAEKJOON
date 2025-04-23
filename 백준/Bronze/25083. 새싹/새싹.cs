2b7c4e163130dff0636a37c76341fa03f7300fa7
lusalord <127508413+lusalord@users.noreply.github.com>
2025-04-23 22:50:49 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,16 @@
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
