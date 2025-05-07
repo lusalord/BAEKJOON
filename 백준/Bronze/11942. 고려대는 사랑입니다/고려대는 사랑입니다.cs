a8d29e3cfb69826bd1e8c180e97f74dbe6cddf4c
lusalord <127508413+lusalord@users.noreply.github.com>
2025-05-07 23:25:55 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 44 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cs
@@ -0,0 +1,12 @@
+using System.Text;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("고려대학교");
+        }
+    }
+}
