94b254a987de03b6bea1ffe261a242c4c756e912
lusalord <127508413+lusalord@users.noreply.github.com>
2025-05-14 23:54:34 +0900

    [Bronze V] Title: Rook, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ___  ___  ___");
+            Console.WriteLine("  | |__| |__| |");
+            Console.WriteLine("  |           |");
+            Console.WriteLine("   \\_________/");
+            Console.WriteLine("    \\_______/");
+            Console.WriteLine("     |     |");
+            Console.WriteLine("     |     |");
+            Console.WriteLine("     |     |");
+            Console.WriteLine("     |     |");
+            Console.WriteLine("     |_____|");
+            Console.WriteLine("  __/       \\__");
+            Console.WriteLine(" /             \\");
+            Console.WriteLine("/_______________\\");
+        }
+    }
+}
