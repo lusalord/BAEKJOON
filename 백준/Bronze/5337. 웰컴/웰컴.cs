f045714a674fc17cdd1961587cf80493877290af
lusalord <127508413+lusalord@users.noreply.github.com>
2025-05-18 00:19:25 +0900

    [Bronze V] Title: 웰컴, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".  .   .");
+            Console.WriteLine("|  | _ | _. _ ._ _  _");
+            Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+
+        }
+    }
+}
