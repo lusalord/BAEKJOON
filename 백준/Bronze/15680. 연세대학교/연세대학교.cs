7f05b66bdfad796c5256397b6147bc3368bd4f92
lusalord <127508413+lusalord@users.noreply.github.com>
2025-05-21 23:35:19 +0900

    [Bronze V] Title: 연세대학교, Time: 48 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if(a == 0)
+            {
+                Console.WriteLine("YONSEI");
+            }
+            else if(a == 1)
+            {
+                Console.WriteLine("Leading the Way to the Future");
+            }
+        }
+    }
+}
