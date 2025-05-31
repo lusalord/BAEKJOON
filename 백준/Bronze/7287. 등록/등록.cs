ab8fdc6e0bf1decbf1316812869749d52c8a97e5
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-01 05:38:29 +0900

    [Bronze V] Title: 등록, Time: 40 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("90");
+            Console.WriteLine("lusalord");
+        }
+    }
+}
