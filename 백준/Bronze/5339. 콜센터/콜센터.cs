3fc3d405f610d6cb6fef8d0cc653a23824efdedd
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-07 23:28:18 +0900

    [Bronze V] Title: 콜센터, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /~\\");
+            Console.WriteLine("    ( oo|");
+            Console.WriteLine("    _\\=/_");
+            Console.WriteLine("   /  _  \\");
+            Console.WriteLine("  //|/.\\|\\\\");
+            Console.WriteLine(" ||  \\ /  ||");
+            Console.WriteLine("============");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+        }
+    }
+}
