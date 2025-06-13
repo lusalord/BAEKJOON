f14cb7454c1bf3090ba80318942b604981bb408a
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-14 02:41:55 +0900

    [Bronze V] Title: Hook, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#  # #### #### #  #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#  # #### #### #  #");
+        }
+    }
+}
