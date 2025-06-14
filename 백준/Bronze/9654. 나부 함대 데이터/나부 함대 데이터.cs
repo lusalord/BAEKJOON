889841164ea8a34ebac824024db2298417c51347
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-15 01:46:00 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+            Console.WriteLine("N2 Bomber      Heavy Fighter  Limited    21        ");
+            Console.WriteLine("J-Type 327     Light Combat   Unlimited  1         ");
+            Console.WriteLine("NX Cruiser     Medium Fighter Limited    18        ");
+            Console.WriteLine("N1 Starfighter Medium Fighter Unlimited  25        ");
+            Console.WriteLine("Royal Cruiser  Light Combat   Limited    4         ");
+        }
+    }
+}
