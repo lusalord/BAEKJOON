5676d9a76f74221355abe94ee5557d8e7e2e8cfd
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-19 00:43:29 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5716 ms, Memory: 27152 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger[] a = Array.ConvertAll(Console.ReadLine().Split(), BigInteger.Parse);
+
+            Console.WriteLine(a[0] * a[1]);
+
+            
+        }
+    }
+}
