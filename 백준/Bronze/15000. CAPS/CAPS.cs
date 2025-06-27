19ebaa5af40aa1ac18e3bcfb94c637927bcd8c7b
lusalord <127508413+lusalord@users.noreply.github.com>
2025-06-28 00:23:03 +0900

    [Bronze V] Title: CAPS, Time: 176 ms, Memory: 12452 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            Console.WriteLine(a.ToUpper());
+        }
+        
+    }
+}
